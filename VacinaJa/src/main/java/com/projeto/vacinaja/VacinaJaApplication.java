1d29720dd691570f5d84986ecc4c36302e9654dc
danilocsm <danilocrgm@LAPTOP-9ACBKHIP.localdomain>
2021-05-03 11:09:02 -0300

    added swagger config

--- VacinaJa/src/main/java/com/projeto/vacinaja/VacinaJaApplication.java
+++ VacinaJa/src/main/java/com/projeto/vacinaja/VacinaJaApplication.java
@@ -1,4 +1,4 @@
-package com.example.VacinaJa;
+package com.projeto.vacinaja;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
